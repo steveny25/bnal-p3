6f3f1dbdba349a8c2724eb253ce163c23abce3ff
steveny25 <steveny25@hotmail.com>
2019-09-18 12:01:42 +0800

    Initial commit

---
+++ tool/test01.py
@@ -0,0 +1,8 @@
+# loc = ["张三","李四","王五","赵六"]
+#
+# # 遍历 并返回 指定值  列表=[返回值 for i in 列表]
+# arr = [el[0] for el in loc]
+# print(arr)
+
+result = ["lisi  18610000000", "张三  13800001111"]
+assert "lisi" in result
